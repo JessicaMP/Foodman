ef0b43ea70bb7f0c01ede19f0cda9563b47d1a07
JessicaMP <jessica95.mp@gmail.com>
2017-12-28 18:51:18 -0500

    Agregando input de filtrado

--- js/app.js
+++ js/app.js
@@ -2,5 +2,7 @@
 $(function() {
   setTimeout(function() {
     $('#splash').fadeOut();
+    $('#principal-header').removeClass('hide');
+    $('#principal-body').removeClass('hide');
   }, 3500);
 });
