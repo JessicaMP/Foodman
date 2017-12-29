2466640fda804dac7464b9b40494de41d2025294
JessicaMP <jessica95.mp@gmail.com>
2017-12-29 14:42:13 -0500

    Mapa tamaño

--- js/app.js
+++ js/app.js
@@ -10,8 +10,9 @@ $(function() {
 /*__________________MAPA________________*/
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 6
+    center: {lat: -34.397,
+    lng: 150.644},
+    zoom: 15
     });
     var infoWindow = new google.maps.InfoWindow({map: map});
 
@@ -21,7 +22,7 @@ function initMap() {
         var pos = {
             lat: position.coords.latitude,
             lng: position.coords.longitude
-            };
+          };
 
             infoWindow.setPosition(pos);
             infoWindow.setContent('Location found.');
