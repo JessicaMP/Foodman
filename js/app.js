54d77eaa3f9d3bd89650d55bce016217bb4201db
JessicaMP <jessica95.mp@gmail.com>
2017-12-29 12:07:42 -0500

    Agregando ready

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ $(function() {
 });
 
 /*__________________MAPA________________*/
-
+$(document).ready(initMap);
 
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
